a2d40e3466359b43dbda0c879b71d4e958bf2d7f
Grant Jenks <grant.jenks@gmail.com>
2024-02-02 10:00:11 -0800

    Update hosts and urls

--- www/urls.py
+++ www/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("admin/", admin.site.urls),
-    path('search/', include('djangoinstantsearch.urls')),
+    path('search/api/', include('djangoinstantsearch.urls')),
 ]
