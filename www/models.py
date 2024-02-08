2ca5417b5ea981aeede13a441b42d3159ae15cc6
Grant Jenks <grant.jenks@gmail.com>
2024-02-07 21:05:57 -0800

    Add Book model and migration

--- www/models.py
+++ www/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 from django.contrib.postgres.indexes import GinIndex
 from django.contrib.postgres.search import SearchVector
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    authors = JSONField()
+    authors = models.JSONField()
     publication_year = models.IntegerField()
     average_rating = models.DecimalField(max_digits=3, decimal_places=2)
     image_url = models.URLField(max_length=1024)
