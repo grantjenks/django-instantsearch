a85e7bf24bb9d70fa4fd1cc37efa0ff4446f645d
Grant Jenks <grant.jenks@gmail.com>
2024-02-07 21:29:42 -0800

    Switch to postgres

--- www/settings.py
+++ www/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'www.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoinstantsearch',
+        'CONN_MAX_AGE': 600,
     }
 }
 
