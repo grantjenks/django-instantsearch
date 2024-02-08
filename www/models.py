3dc6ac2161bd344afddd2f9e33d26bbe5a5a95d9
Grant Jenks <grant.jenks@gmail.com>
2024-02-07 21:05:10 -0800

    Create the book model

--- www/models.py
+++ www/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVector
 
 
-class Document:
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    authors = JSONField()
+    publication_year = models.IntegerField()
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2)
+    image_url = models.URLField(max_length=1024)
+    ratings_count = models.BigIntegerField()
+    synopsis = models.TextField()
+
+    class Meta:
+        indexes = [
+            GinIndex(
+                SearchVector('title', 'synopsis', config='english'),
+                name='book_search_vector_index',
+            ),
+        ]
+
+    def __str__(self):
+        return self.title
